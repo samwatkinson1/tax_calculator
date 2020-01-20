c8c441eec58cbb1684b60bb39fe2df8b45ba6ce4
Sam <samwatkinson@outlook.com>
2020-01-20 12:19:27 +0000

    Added alternative tax calculating functionality

--- exercises/java/src/main/java/tax/DefaultTaxCalculator.java
+++ exercises/java/src/main/java/tax/DefaultTaxCalculator.java
@@ -11,8 +11,10 @@ public class DefaultTaxCalculator extends TaxCalculator {
             return petrolCo2Calculator(vehicleCo2Emissions);
         } else if (vehicleFuelType == FuelType.DIESEL) {
             return dieselCo2Calculator(vehicleCo2Emissions);
+        } else if (vehicleFuelType == FuelType.ALTERNATIVE_FUEL) {
+            return alternateCo2Calculator(vehicleCo2Emissions);
         } else {
-            return 9999;
+            return 0;
         }
     }
 
@@ -69,6 +71,34 @@ public class DefaultTaxCalculator extends TaxCalculator {
             return 105;
         } else if (co2 >= 1) {
             return 25;
+        } else {
+            return 0;
+        }
+    }
+
+    private int alternateCo2Calculator(int co2) {
+        if (co2 > 255) {
+            return 2060;
+        } else if (co2 >= 226) {
+            return 1750;
+        } else if (co2 >= 191) {
+            return 1230;
+        } else if (co2 >= 171) {
+            return 820;
+        } else if (co2 >= 151) {
+            return 505;
+        } else if (co2 >= 131) {
+            return 195;
+        } else if (co2 >= 111) {
+            return 155;
+        } else if (co2 >= 101) {
+            return 135;
+        } else if (co2 >= 91) {
+            return 115;
+        } else if (co2 >= 76) {
+            return 95;
+        } else if (co2 >= 51) {
+            return 15;
         } else {
             return 0;
         }
--- exercises/java/src/test/java/tax/TaxCalculatorAlternativeFuelTest.java
+++ exercises/java/src/test/java/tax/TaxCalculatorAlternativeFuelTest.java
@@ -11,7 +11,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static tax.FuelType.ALTERNATIVE_FUEL;
 import static tax.FuelType.ELECTRIC;
 
-@Ignore
 public class TaxCalculatorAlternativeFuelTest {
 
     private static final LocalDate FIRST_OF_JAN_2019 = LocalDate.of(2019, JANUARY, 1);
@@ -19,7 +18,7 @@ public class TaxCalculatorAlternativeFuelTest {
 
     @Before
     public void setUp() {
-        taxCalculator = new DummyTaxCalculator();
+        taxCalculator = new DefaultTaxCalculator();
     }
 
     @Test
